a9d91bc25c8408111a3cd6d4c90e35f8c12c5155
myway00 <junior0614@naver.com>
2023-09-05 13:39:47 +0900

    FIX : JDBC Url target to localhost

--- jdbc-practices/src/main/java/bookmall/common/JdbcVar.java
+++ jdbc-practices/src/main/java/bookmall/common/JdbcVar.java
@@ -2,7 +2,7 @@ package bookmall.common;
 
 public class JdbcVar {
 
-    public static final String URL = "jdbc:mariadb://192.168.%:3307/bookmall?charset=utf8";
+    public static final String URL = "jdbc:mariadb://localhost:3307/bookmall?charset=utf8";
     public static final String USER = "bookmall";
     public static final String PASSWORD = "bookmall";
     public static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
